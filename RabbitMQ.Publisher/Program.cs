7728df59e068c438d30bf03b2b35b5b4e4463a46
Ender Bakır <enderyx_123@hotmail.com>
2024-01-31 13:42:14 +0300

    Connecting rabbitmq with docker
    Creating the message queue system between Publisher and Subscriber Project.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if we have a cloud rabbitmq we must use this method in Connection 
+            //var factory = new ConnectionFactory() {factory.uri = new Uri("Cloud Link")}
+
+            var factory = new ConnectionFactory()//Connect RabbitMQ with Docker Container.
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue" , true , false , false);
+
+            string message = "hello rabbit";
+            var messagebody=Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messagebody);
+
+            Console.WriteLine("Mesaj Gönderilmiştir");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if we have a cloud rabbitmq we must use this method in Connection 
+            //var factory = new ConnectionFactory() {factory.uri = new Uri("Cloud Link")}
+
+            var factory = new ConnectionFactory()//Connect RabbitMQ with Docker Container.
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);//if u have same queue in publisher this method unncessary but harmless , and u must use same options for queue.
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Uyarı" + message);
+            };
+
+
+
+
+            Console.ReadLine();
+        }
+
+      
+    }
+}
